0d032a8790aa4ab7c02b1884acd95a552b0018e5
Hilmi Güner <hilmi.guner@hotmail.com>
2022-06-14 00:18:18 +0300

    Initial

--- Module Json/main.py
+++ Module Json/main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+person_string = '{"name": "Ali", "languages": ["python", "c#"]}'  # It looks like a dictionary but it is just a string.
+# With "json.loads(string str)" we can turn strings into dictionaries.
 
+dict1FromString = json.loads(person_string)
+print(type(dict1FromString))  # As we can see it is a dictionary object.
+print(dict1FromString)
+print("Name:", dict1FromString["name"])
+print("Languages:", dict1FromString["languages"])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(50*"*")
 
+# "json.loads()" turns strings to dictionaries, but "json.load()" reads a json file and turns it to dictionary.
+with open("person.json") as file:  # This code block closes the file automatically at the end of itself.
+    dict2FromJsonFile = json.load(file)
+print(type(dict2FromJsonFile))
+print("Name:", dict2FromJsonFile["name"])
+print("Languages:", dict2FromJsonFile["languages"])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(50*"*")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# "json.dumps(dict obj)" turns the dict objects to json strings.
+person_dict = {
+  "name": "Ali",
+  "languages": ["python", "c#"]
+}
+
+str1FromDict = json.dumps(person_dict)
+print(type(str1FromDict))
+print(str1FromDict)
+
+# "json.dump(dict obj, File file)" takes a dictionary and write it to the json file as a json string.
+person_dict2 = {
+    "name": "Hilmi",
+    "age": "21",
+    "city": "Istanbul"
+}
+with open("dictToJson.json", "w") as file:
+    json.dump(person_dict2, file)  # If you look at the "dictToJson.json" file, you can see the "person_dict2" as a json string.
+
