2da8780693759e6613b8aa94f62bc1ef34cecfaf
Hilmi Güner <hilmi.guner@hotmail.com>
2022-08-12 18:13:14 +0300

    Initial

--- Modules/Module BeautifulSoup/main.py
+++ Modules/Module BeautifulSoup/main.py
@@ -3,7 +3,7 @@ def printText(str1: str, str2: str):
     print(str2)
     print(50*"*")
 
-with open("index.html", "r") as file:
+with open("Modules/Module BeautifulSoup/index.html", "r") as file:
     html_doc = file.read()
 
 from bs4 import BeautifulSoup
