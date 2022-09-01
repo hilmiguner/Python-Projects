edfce12985248cfc865a7aab087c49a6dcc645da
Hilmi Güner <hilmi.guner@hotmail.com>
2022-09-02 01:32:32 +0300

    Initial

--- Algorithms/Linear Search/main.py
+++ Algorithms/Linear Search/main.py
@@ -1,8 +1,8 @@
 liste = [31, 12, 24, 36, 48, 55, 3]
 
 def sequentielSearch(x:list, y:int):
-    for i in range(0, len(x)):
-        if x[i] == y:
+    for data in x:
+        if data == y:
             return True
     return False
 
