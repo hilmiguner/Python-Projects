18607a7c1bb92025ba588d20152295229907ab28
Hilmi Güner <hilmi.guner@hotmail.com>
2022-10-04 11:10:54 +0300

    Initial

--- Algorithms/Bubble Sort/main.py
+++ Algorithms/Bubble Sort/main.py
@@ -2,16 +2,11 @@ liste = [31, 12, 24, 36, 48, 55, 3]
 
 length = len(liste)
 
-def swap(x, a, b):
-    temp = x[a]
-    x[a] = x[b]
-    x[b] = temp
-
 def bubbleSort(x:list, y:int):
     for i in range(0, length-1):
         for k in range(0, length-i-1):
             if x[k] > x[k+1]:
-                swap(x, k, k+1)
+                x[k], x[k+1] = x[k+1], x[k]
     return x
 
 print(bubbleSort(liste, length))
